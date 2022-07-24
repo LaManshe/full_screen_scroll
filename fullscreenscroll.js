02e10dfafb0774e5c57a5704bb3f2c1395f7dd2d
LaManshe <ilm9816@gmail.com>
2022-07-24 21:13:04 +0500

    added: no out, return to start option

--- fullscreenscroll.js
+++ fullscreenscroll.js
@@ -1,30 +1,40 @@
 export default class FSS{
-    constructor(containerQuery, screenQuery, timeOffset){
+    constructor(containerQuery, screenQuery, timeOffset = 2000, returnToStartWhenEnd = true){
         this.containerDOM = document.querySelector(containerQuery);
         this.screensDOM = document.querySelectorAll(screenQuery);
         this.timeOffset = timeOffset;
+        this.returnToStartWhenEnd = returnToStartWhenEnd;
 
         this.Y = 0;
         this.scrollAllow = true;
+
+        history.scrollRestoration = "manual";
     }
 
     initialize(){
-
         var me = this;
         function handleScroll(event){
-            if(me.scrollAllow){
-                me.scrollAllow = false;
-
-                me.Y += -event.deltaY;
-
-                me.containerDOM.style.transform = "translateY(" + me.Y + "vh)";
-        
-                setTimeout(function(){me.scrollAllow = true}, me.timeOffset);
+            if(!me.scrollAllow){
+                return;
             }
+            me.Y += -event.deltaY;
+
+            if(me.Y <= me.screensDOM.length * (-100) || me.Y > 0){
+                if(me.returnToStartWhenEnd){
+                    me.Y = 0;
+                }
+                else{
+                    me.Y -= -event.deltaY;
+                }
+            }
+
+            me.scrollAllow = false;
+
+            me.containerDOM.style.transform = "translateY(" + me.Y + "vh)";
+    
+            setTimeout(function(){me.scrollAllow = true}, me.timeOffset);
         }
 
         window.addEventListener('wheel', handleScroll);
     }
-
-    
 }
